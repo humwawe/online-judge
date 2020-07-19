50eb1e354aaa1e0f90de3cf9aace003b9e5b2d4d
humwawe <wawe8963098@163.com>
2020-07-19 18:11:21 +0800

    fast io

---
+++ src/main/java/fast/io/FastReader.java
@@ -0,0 +1,47 @@
+package fast.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author hum
+ */
+public class FastReader {
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer tokenizer = new StringTokenizer("");
+
+    private String innerNextLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    public boolean hasNext() {
+        while (!tokenizer.hasMoreTokens()) {
+            String nextLine = innerNextLine();
+            if (nextLine == null) {
+                return false;
+            }
+            tokenizer = new StringTokenizer(nextLine);
+        }
+        return true;
+    }
+
+    public String nextLine() {
+        tokenizer = new StringTokenizer("");
+        return innerNextLine();
+    }
+
+    public String next() {
+        hasNext();
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
