8cdf023823bd97dc62ef04314ec44cf3ddeba459
humwawe <wawe8963098@163.com>
2021-06-05 22:15:58 +0800

    边的删减 最大差值 三元一次方程

---
+++ src/main/java/acwing/contest/weekly/c2/TaskB.java
@@ -0,0 +1,33 @@
+package acwing.contest.weekly.c2;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+import java.util.Arrays;
+
+public class TaskB {
+    int n, k;
+
+    // 最大的k+1个数
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        n = in.nextInt();
+        k = in.nextInt();
+        int[] a = in.nextIntArray(n);
+        Arrays.sort(a);
+        if (n == 1) {
+            out.println(0);
+            return;
+        }
+        int start = n - 2;
+        long sum = a[n - 1];
+        while (start >= 0 && k-- > 0) {
+            if (a[start] > 0) {
+                sum += a[start];
+                start--;
+            } else {
+                break;
+            }
+        }
+        out.println(sum);
+    }
+}
