016d7ce179bd724334571588e79455d3d81d38ae
humwawe <wawe8963098@163.com>
2021-08-10 21:54:36 +0800

    c737

---
+++ src/main/java/codeforces/c737/AEzzatAndTwoSubsequences.java
@@ -0,0 +1,19 @@
+package codeforces.c737;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+public class AEzzatAndTwoSubsequences {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        double[] x = new double[n];
+        double sum = 0;
+        double max = -Double.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            x[i] = in.nextInt();
+            max = Math.max(max, x[i]);
+            sum += x[i];
+        }
+        out.println(max + (sum - max) / (n - 1));
+    }
+}
---
+++ src/main/java/codeforces/c737/BMoamenAndKSubarrays.java
@@ -0,0 +1,33 @@
+package codeforces.c737;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+import java.util.TreeSet;
+
+public class BMoamenAndKSubarrays {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        TreeSet<Integer> set = new TreeSet<>();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+            set.add(a[i]);
+        }
+        int cnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            Integer ceiling = set.ceiling(a[i] + 1);
+            if (ceiling != null && ceiling == a[i + 1]) {
+                continue;
+            } else {
+                cnt++;
+            }
+        }
+        if (cnt < k) {
+            out.println("Yes");
+        } else {
+            out.println("No");
+        }
+    }
+}
