f3d96d36f7958404989e1e1e1dc9a5ede727370d
humwawe <wawe8963098@163.com>
2021-09-02 00:41:51 +0800

    dds

--- src/main/java/codeforces/dds/CCompressedBracketSequence.java
+++ src/main/java/codeforces/dds/CCompressedBracketSequence.java
@@ -4,64 +4,36 @@ import fast.io.InputReader;
 import fast.io.OutputWriter;
 
 public class CCompressedBracketSequence {
-    // todo
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         int n = in.nextInt();
-        int[] a = in.nextIntArray(n);
-        long[] dp = new long[n];
+        long[] a = in.nextLongArray(n);
         long res = 0;
 
-        for (int i = 1; i < n; i += 2) {
-            if (a[i] < a[i - 1]) {
-                dp[i] = a[i];
-                a[i - 1] -= a[i];
-                a[i] = 0;
-            } else if (a[i] == a[i - 1]) {
-                dp[i] = a[i];
-                a[i] = 0;
-                a[i - 1] = 0;
-                if (i - 2 >= 0 && a[i - 2] >= 0) {
-                    dp[i] += dp[i - 2];
-                }
-            } else {
-                dp[i] = a[i - 1];
-                a[i] -= a[i - 1];
-                a[i - 1] = 0;
-                int j = i - 2;
-                boolean f = true;
-                for (; j >= 0; j -= 2) {
-                    if (a[j] == -1) {
-                        break;
-                    }
-                    if (f) {
-                        dp[i] += dp[j];
-                        f = false;
-                    }
-
-                    if (a[i] == 0) {
-                        break;
-                    }
-                    if (a[i] >= a[j - 1]) {
-                        if (a[j - 1] > 0) {
-                            f = true;
-                        }
-                        a[i] -= a[j - 1];
-                        dp[i] += a[j - 1];
-                        a[j - 1] = 0;
+        for (int i = 0; i < n; i += 2) {
+            long begin = a[i];
+            long sum = 0;
+            boolean first = true;
+            for (int j = i + 1; j < n; j++) {
+                if (j % 2 == 0) {
+                    sum += a[j];
+                } else {
+                    if (a[j] < sum) {
+                        sum -= a[j];
                     } else {
-                        dp[i] += a[i];
-                        a[i] = 0;
-                        a[j - 1] -= a[i];
-                        f = true;
+                        res += Math.min(begin, a[j] - sum);
+                        if (first) {
+                            first = false;
+                        } else {
+                            res++;
+                        }
+                        begin -= a[j] - sum;
+                        sum = 0;
+                        if (begin < 0) {
+                            break;
+                        }
                     }
-                }
-
-                if (a[i] > 0) {
-                    a[i] = -1;
                 }
             }
-
-            res += dp[i];
         }
         out.println(res);
     }
--- src/main/java/codeforces/dds/DTakeAGuess.java
+++ src/main/java/codeforces/dds/DTakeAGuess.java
@@ -3,18 +3,50 @@ package codeforces.dds;
 import fast.io.InputReader;
 import fast.io.OutputWriter;
 
+import java.util.Arrays;
+
+// a+b=(a&b)<<1+(a^b)
+// a+b=(a&b)+(a|b)
 public class DTakeAGuess {
-    // todo
+
+    InputReader in;
+    OutputWriter out;
+
     public void solve(int testNumber, InputReader in, OutputWriter out) {
+        this.in = in;
+        this.out = out;
         int n = in.nextInt();
         int k = in.nextInt();
         int[] a = new int[n];
-        int last = 1;
-        for (int i = 2; i <= n; i++) {
-            out.println("and " + last + " " + i);
-            out.flush();
-            last = in.nextInt();
-        }
+        int s12 = add(1, 2) + or(1, 2);
+        int s13 = add(1, 3) + or(1, 3);
+        int s23 = add(2, 3) + or(2, 3);
+        a[0] = (s12 + s13 - s23) / 2;
+        a[1] = s13 - a[0];
+        a[2] = s23 - a[1];
 
+        for (int i = 3; i < n; i++) {
+            int s = add(1, i + 1) + or(1, i + 1);
+            a[i] = s - a[0];
+        }
+        Arrays.sort(a);
+        finish(a[k - 1]);
+    }
+
+    int add(int x, int y) {
+        out.println("and " + x + " " + y);
+        out.flush();
+        return in.nextInt();
+    }
+
+    int or(int x, int y) {
+        out.println("or " + x + " " + y);
+        out.flush();
+        return in.nextInt();
+    }
+
+    void finish(int x) {
+        out.println("finish " + x);
+        out.flush();
     }
 }
