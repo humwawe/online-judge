bf608cb547191465e76722bd72ce42e7e858afb7
humwawe <wawe8963098@163.com>
2021-09-08 00:22:05 +0800

    c742

--- src/main/java/codeforces/c742/DExpressionEvaluationError.java
+++ src/main/java/codeforces/c742/DExpressionEvaluationError.java
@@ -4,31 +4,20 @@ import fast.io.InputReader;
 import fast.io.OutputWriter;
 
 public class DExpressionEvaluationError {
-    //     todo
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         int s = in.nextInt();
-
         int n = in.nextInt();
         String ss = String.valueOf(s);
-        int idx = 1;
-        while (Integer.parseInt(ss.substring(0, idx)) < n) {
-            idx++;
+        int pw = (int) 1e9;
+        while (n > 1) {
+            while (s - pw < n - 1) {
+                pw /= 10;
+            }
+            s -= pw;
+            n--;
+            out.print(pw + " ");
         }
-        String last = ss.substring(idx);
-        String tmp = "";
-        for (int i = 0; i < last.length(); i++) {
-            tmp += "0";
-        }
-        int sum = Integer.parseInt(ss.substring(0, idx));
-        for (int i = 0; i < n - 1; i++) {
-            out.print(1);
-            out.print(tmp);
-            out.print(" ");
-            sum -= 1;
-        }
-        out.print(sum);
-        out.print(last);
-        out.println();
+        out.println(s);
     }
 
 }
