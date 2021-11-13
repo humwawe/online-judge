8b98301b6b5403c1ea47e1bb7ec176d1e36ab02a
humwawe <wawe8963098@163.com>
2021-11-13 22:20:45 +0800

    找规律输出 铁路与公路 染色

---
+++ src/main/java/acwing/contest/weekly/c25/TaskA.java
@@ -0,0 +1,22 @@
+package acwing.contest.weekly.c25;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+public class TaskA {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 2 == 0) {
+                out.print("I hate that ");
+            } else {
+                out.print("I love that ");
+            }
+        }
+        if (n % 2 == 0) {
+            out.print("I love it");
+        } else {
+            out.print("I hate it");
+        }
+    }
+}
