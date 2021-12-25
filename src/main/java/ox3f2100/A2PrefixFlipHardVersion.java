aabd58e9990a76c48b7d2e9e5f844c4caf7168e3
humwawe <wawe8963098@163.com>
2021-12-25 18:15:04 +0800

    1371D 1381A2

---
+++ src/main/java/ox3f2100/A2PrefixFlipHardVersion.java
@@ -0,0 +1,42 @@
+package ox3f2100;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A2PrefixFlipHardVersion {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        String a = in.nextString();
+        String b = in.nextString();
+
+        List<Integer> list = new ArrayList<>();
+        int j = 0;
+        int k = n - 1;
+        boolean f = true;
+        for (int i = n - 1; i >= 0; i--) {
+            if (f) {
+                if (b.charAt(i) == a.charAt(j)) {
+                    list.add(1);
+                }
+                list.add(i + 1);
+                j++;
+                f = false;
+            } else {
+                if (b.charAt(i) != a.charAt(k)) {
+                    list.add(1);
+                }
+                list.add(i + 1);
+                k--;
+                f = true;
+            }
+        }
+        out.print(list.size());
+        for (Integer i : list) {
+            out.print(" " + i);
+        }
+        out.println();
+    }
+}
---
+++ src/main/java/ox3f2100/DGrid00100.java
@@ -0,0 +1,42 @@
+package ox3f2100;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+public class DGrid00100 {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[][] res = new int[n][n];
+        int col = 0;
+        int cnt = 0;
+        f:
+        while (k > 0) {
+            int i = 0;
+            int j = col++;
+            for (int l = 0; l < n; l++) {
+                res[i][j] = 1;
+                cnt++;
+                if (cnt == k) {
+                    break f;
+                }
+                i++;
+                j++;
+                if (j >= n) {
+                    j -= n;
+                }
+            }
+        }
+        if (k % n != 0) {
+            out.println(2);
+        } else {
+            out.println(0);
+        }
+        for (int l = 0; l < n; l++) {
+            for (int m = 0; m < n; m++) {
+                out.print(res[l][m]);
+            }
+            out.println();
+        }
+    }
+}
