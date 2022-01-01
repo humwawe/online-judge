87e6ac01650e4740443fc42bb7933ae3b794ac08
humwawe <wawe8963098@163.com>
2022-01-01 17:27:35 +0800

    c763

--- src/main/java/codeforces/c763/CBalancedStoneHeaps.java
+++ src/main/java/codeforces/c763/CBalancedStoneHeaps.java
@@ -7,7 +7,6 @@ public class CBalancedStoneHeaps {
     int n;
     int[] h;
 
-    // todo
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         n = in.nextInt();
         h = in.nextIntArray(n);
@@ -25,33 +24,15 @@ public class CBalancedStoneHeaps {
     }
 
     private boolean check(int mid) {
-        int[] a = new int[n];
-        if (h[n - 1] >= mid) {
-            int d = (h[n - 1] - mid) / 3;
-            a[n - 2] += d;
-            a[n - 3] += 2 * d;
-        } else {
-            return false;
-        }
-
-        for (int i = n - 2; i >= 2; i--) {
-            int d = h[i] / 3;
-            if (d > 0) {
-                a[i - 1] += d;
-                a[i - 2] += 2 * d;
-            }
-        }
-        if (a[0] + h[0] < mid) {
-            return false;
-        }
-        if (a[1] + h[1] < mid) {
-            return false;
-        }
-        for (int i = 2; i < n - 1; i++) {
-            if (a[i] + h[i] % 3 < mid) {
+        int[] a = h.clone();
+        for (int i = n - 1; i >= 2; i--) {
+            if (a[i] < mid) {
                 return false;
             }
+            int d = Math.min(h[i], a[i] - mid) / 3;
+            a[i - 1] += d;
+            a[i - 2] += 2 * d;
         }
-        return true;
+        return a[0] >= mid && a[1] >= mid;
     }
 }
