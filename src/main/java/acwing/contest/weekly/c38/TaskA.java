7090cf87d75acfa6548835825b694219496ff35a
humwawe <wawe8963098@163.com>
2022-02-12 19:22:54 +0800

    删点 两种操作 截断数列

---
+++ src/main/java/acwing/contest/weekly/c38/TaskA.java
@@ -0,0 +1,26 @@
+package acwing.contest.weekly.c38;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+public class TaskA {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            if (a > 0) {
+                r++;
+            } else {
+                l++;
+            }
+        }
+        if (l > 1 && r > 1) {
+            out.println("No");
+        } else {
+            out.println("Yes");
+        }
+    }
+}
