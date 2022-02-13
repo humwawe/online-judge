b3f0e2228e8e7b6bba824b8d55db40e1a903566b
humwawe <wawe8963098@163.com>
2022-02-13 10:23:37 +0800

    736A

---
+++ src/main/java/ox3f2100/CTennisChampionship.java
@@ -0,0 +1,23 @@
+package ox3f2100;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+public class CTennisChampionship {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        long n = in.nextLong();
+        long a = 2;
+        long b = 1;
+        long res = 0;
+        while (true) {
+            if (a > n) {
+                out.println(res);
+                return;
+            }
+            res++;
+            long c = a + b;
+            b = a;
+            a = c;
+        }
+    }
+}
