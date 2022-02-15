50be636673491982eaab67545fd0b55e36fd777d
humwawe <wawe8963098@163.com>
2022-02-16 00:16:52 +0800

    c771

--- src/main/java/codeforces/c771/DBigBrush.java
+++ src/main/java/codeforces/c771/DBigBrush.java
@@ -3,10 +3,7 @@ package codeforces.c771;
 import fast.io.InputReader;
 import fast.io.OutputWriter;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class DBigBrush {
     int n, m;
@@ -23,46 +20,79 @@ public class DBigBrush {
                 a[i][j] = in.nextInt();
             }
         }
-        int x = -1;
-        int y = -1;
-
+        Queue<int[]> queue = new ArrayDeque<>();
+        List<int[]> res = new ArrayList<>();
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < m - 1; j++) {
                 if (check(i, j)) {
-                    x = i;
-                    y = j;
-                    break;
+                    queue.add(new int[]{i, j});
                 }
             }
         }
-        if (x == -1) {
-            out.println(-1);
-            return;
-        }
-        Queue<int[]> queue = new ArrayDeque<>();
-        List<int[]> res = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                queue.add(new int[]{x + i, y + j});
-                vis[x + i][y + j] = true;
-            }
-        }
-        res.add(new int[]{x, y, a[x][y]});
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
-            if (valid(poll[0], poll[1]) && !vis[poll[0]][poll[1]]) {
-
+            int x = poll[0];
+            int y = poll[1];
+            int c = -1;
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    if (a[x + i][y + j] != -1) {
+                        c = a[x + i][y + j];
+                    }
+                }
             }
+            if (c != -1) {
+                res.add(new int[]{x, y, c});
+                for (int i = 0; i < 2; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        a[x + i][y + j] = -1;
+                    }
+                }
+
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        int nx = x + i;
+                        int ny = y + j;
+                        if (valid(nx, ny)) {
+                            if (check(nx, ny)) {
+                                queue.add(new int[]{nx, ny});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (a[i][j] != -1) {
+                    out.println(-1);
+                    return;
+                }
+            }
+        }
+        out.println(res.size());
+        for (int i = res.size() - 1; i >= 0; i--) {
+            int[] xyc = res.get(i);
+            for (int j = 0; j < 2; j++) {
+                xyc[j] += 1;
+            }
+            out.println(xyc);
         }
 
     }
 
-    private boolean check(int i, int j) {
-        if (valid(i, j)) {
-            return a[i][j] == a[i + 1][j] && a[i][j] == a[i][j + 1] && a[i][j] == a[i + 1][j + 1];
+    private boolean check(int x, int y) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (a[i + x][j + y] != -1) {
+                    set.add(a[i + x][j + y]);
+                }
+            }
         }
-        return false;
+        return set.size() == 1;
     }
 
     private boolean valid(int i, int j) {
