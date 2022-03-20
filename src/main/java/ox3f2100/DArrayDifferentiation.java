81d82f5583fb9a8656f8712152f9914904654810
humwawe <wawe8963098@163.com>
2022-03-20 19:36:33 +0800

    1552D

---
+++ src/main/java/ox3f2100/DArrayDifferentiation.java
@@ -0,0 +1,34 @@
+package ox3f2100;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DArrayDifferentiation {
+  public void solve(int testNumber, InputReader in, OutputWriter out) {
+    int n = in.nextInt();
+    int[] a = new int[n];
+    Set<Integer> set = new HashSet<>();
+    for (int i = 0; i < n; i++) {
+      a[i] = in.nextInt();
+      set.add(a[i]);
+    }
+    for (int i = 0; i < 1 << n; i++) {
+      int sum = 0;
+      for (int j = 0; j < n; j++) {
+        if ((i >> j & 1) == 1) {
+          sum += a[j];
+        }
+      }
+      set.add(sum);
+    }
+    if (set.size() == 1 << n) {1552D
+      out.println("NO");
+    } else {
+      out.println("YES");
+    }
+
+  }
+}
