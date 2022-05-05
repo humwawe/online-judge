555d2ced6c854a9ced389cac737c0c6ae30153ea
humwawe <wawe8963098@163.com>
2022-05-05 18:40:47 +0800

    1659D

--- src/main/java/ox3f2100/DReverseSortSum.java
+++ src/main/java/ox3f2100/DReverseSortSum.java
@@ -6,53 +6,31 @@ import fast.io.OutputWriter;
 import java.util.Arrays;
 
 public class DReverseSortSum {
-  int N = 200005;
-  int[] t = new int[N];
 
-  int lowbit(int x) {
-    return x & -x;
-  }
-
-  void add(int x, int c) {
-    for (int i = x; i <= n; i += lowbit(i)) {
-      t[i] += c;
-    }
-  }
-
-  int sum(int x) {
-    int res = 0;
-    for (int i = x; i > 0; i -= lowbit(i)) {
-      res += t[i];
-    }
-    return res;
-  }
 
   int n;
 
   public void solve(int testNumber, InputReader in, OutputWriter out) {
     n = in.nextInt();
-    Arrays.fill(t, 1, n + 5, 0);
-    int[] a = new int[n + 1];
-    long sum = 0;
-    for (int i = 1; i <= n; i++) {
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++) {
       a[i] = in.nextInt();
-      sum += a[i];
-    }
-    int[] res = new int[n];
-    int k = (int) (sum / n);
-
-    for (int i = n; i > 0; i--) {
-      if (a[i] + sum(i) == i) {
-        res[i - 1] = 1;
-      } else {
-        res[i - 1] = 0;
-      }
-      add(i - k + 1, -1);
-      if (res[i - 1] == 1) {
-        k--;
-      }
     }
 
-    out.println(res);
+    int[] res = new int[n + 1];
+    Arrays.fill(res, 1);
+    int idx = -1;
+    while (idx < n - 1 && a[++idx] == 0) {
+      res[idx] = 0;
+    }
+    for (; idx < n; idx++) {
+      int k = a[idx] - idx * res[idx];
+      res[k + idx] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+      out.print(res[i], "");
+    }
+    out.println();
   }
 }
