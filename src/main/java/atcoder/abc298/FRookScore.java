ce834125b72b50f9200fb2809c765b4c25201495
humwawe <wawe8963098@163.com>
2023-04-16 20:33:33 +0800

    abc298

--- src/main/java/atcoder/abc298/FRookScore.java
+++ src/main/java/atcoder/abc298/FRookScore.java
@@ -9,50 +9,84 @@ public class FRookScore {
 
   public void solve(int testNumber, InputReader in, OutputWriter out) {
     int n = in.nextInt();
-    Map<Long, Long> xs = new HashMap<>();
-    Map<Long, Long> ys = new HashMap<>();
-    long[] x = new long[n];
-    long[] y = new long[n];
+    Map<Integer, Long> xs = new HashMap<>();
+    Map<Integer, Long> ys = new HashMap<>();
+    int[] x = new int[n];
+    int[] y = new int[n];
     long[] v = new long[n];
-    Map<Long, Set<Long>> mp = new HashMap<>();
+    Map<Integer, Set<Integer>> mp = new HashMap<>();
     for (int i = 0; i < n; i++) {
-      x[i] = in.nextLong();
-      y[i] = in.nextLong();
+      x[i] = in.nextInt();
+      y[i] = in.nextInt();
       v[i] = in.nextLong();
-      mp.computeIfAbsent(x[i], key -> new HashSet<>()).add(y[i]);
+      mp.computeIfAbsent(x[i], key -> new HashSet<>()).add(i);
       xs.merge(x[i], v[i], Long::sum);
       ys.merge(y[i], v[i], Long::sum);
     }
-    List<Nod> list = new ArrayList<>();
-    for (Map.Entry<Long, Long> entry : ys.entrySet()) {
-      list.add(new Nod(entry.getKey(), entry.getValue()));
+    PrioritySet<Long> prioritySet = new PrioritySet<>((a, b) -> Long.compare(b, a));
+
+    for (Long value : ys.values()) {
+      prioritySet.add(value);
     }
-    list.sort(Comparator.comparingLong(p -> -p.v));
-    long ans = 0;
-    for (int i = 0; i < n; i++) {
-      ans = Math.max(ans, xs.get(x[i]) + ys.get(y[i]) - v[i]);
-    }
-    for (Map.Entry<Long, Set<Long>> entry : mp.entrySet()) {
-      long xv = entry.getKey();
-      for (Nod p : list) {
-        if (entry.getValue().contains(p.c)) {
-          continue;
-        }
-        ans = Math.max(ans, xs.get(xv) + p.v);
-        break;
+
+    long res = 0;
+
+    for (Map.Entry<Integer, Set<Integer>> entry : mp.entrySet()) {
+      int xv = entry.getKey();
+      Set<Integer> idx = entry.getValue();
+      for (Integer integer : idx) {
+        prioritySet.erase(ys.get(y[integer]));
+        prioritySet.add(ys.get(y[integer]) - v[integer]);
+      }
+
+      res = Math.max(res, xs.get(xv) + prioritySet.peek());
+
+      for (Integer integer : idx) {
+        prioritySet.add(ys.get(y[integer]));
+        prioritySet.erase(ys.get(y[integer]) - v[integer]);
       }
     }
-    out.println(ans);
+    out.println(res);
   }
 
-  class Nod {
-    long c;
-    long v;
 
-    public Nod(long c, long v) {
-      this.c = c;
-      this.v = v;
+  class PrioritySet<T> {
+    PriorityQueue<T> base, deleted;
+
+    public PrioritySet(Comparator<? super T> comparator) {
+      base = new PriorityQueue<>(comparator);
+      deleted = new PriorityQueue<>(comparator);
     }
+
+    public PrioritySet() {
+      base = new PriorityQueue<>();
+      deleted = new PriorityQueue<>();
+    }
+
+    void add(T x) {
+      base.add(x);
+    }
+
+    void erase(T x) {
+      deleted.add(x);
+    }
+
+    T peek() {
+      while (!deleted.isEmpty() && deleted.peek().equals(base.peek())) {
+        base.poll();
+        deleted.poll();
+      }
+      return base.peek();
+    }
+
+    T poll() {
+      while (!deleted.isEmpty() && deleted.peek().equals(base.peek())) {
+        base.poll();
+        deleted.poll();
+      }
+      return base.poll();
+    }
+
   }
 
 }
