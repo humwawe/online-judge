7bcb037186becf5065221fae9fb3291a6edccb89
humwawe <wawe8963098@163.com>
2023-07-31 22:40:15 +0800

    cf

---
+++ src/main/java/luoguo/problems/AirConditioner.java
@@ -0,0 +1,44 @@
+package luoguo.problems;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+import java.util.Arrays;
+
+public class AirConditioner {
+  public void solve(int testNumber, InputReader in, OutputWriter out) {
+    int n = in.nextInt();
+    long m = in.nextInt();
+    int[] t = new int[n];
+    int[] l = new int[n];
+    int[] r = new int[n];
+    for (int i = 0; i < n; i++) {
+      t[i] = in.nextInt();
+      l[i] = in.nextInt();
+      r[i] = in.nextInt();
+    }
+
+    Integer[] idx = new Integer[n];
+    Arrays.setAll(idx, i -> i);
+
+    long min = m;
+    long max = m;
+
+    for (int ii = 0; ii < n; ii++) {
+      int i = idx[ii];
+      int d = t[i];
+      if (i >= 1) {
+        d -= t[i - 1];
+      }
+      max = max + d;
+      min = min - d;
+      if (l[i] > max || min > r[i]) {
+        out.println("NO");
+        return;
+      }
+      min = Math.max(min, l[i]);
+      max = Math.min(max, r[i]);
+    }
+    out.println("YES");
+  }
+}
---
+++ src/main/java/luoguo/problems/Present.java
@@ -0,0 +1,48 @@
+package luoguo.problems;
+
+import fast.io.InputReader;
+import fast.io.OutputWriter;
+
+import java.util.Arrays;
+
+public class Present {
+  OutputWriter out;
+
+  public void solve(int testNumber, InputReader in, OutputWriter out) {
+    int n = in.nextInt();
+    int[] a = in.nextInt(n);
+    this.out = out;
+    int[] b = new int[n];
+    int res = 0;
+    for (int i = 0; i <= 25; i++) {
+      for (int j = 0; j < n; j++) {
+        b[j] = a[j] % (1 << (i + 1));
+      }
+      Arrays.sort(b);
+      int cnt = helper(b, 1 << i, (1 << (i + 1)) - 1) ^ helper(b, 3 << i, (1 << (i + 2)) - 2);
+      res |= cnt * (1 << i);
+    }
+    out.println(res);
+
+  }
+
+  private int helper(int[] b, int i, int j) {
+    long cnt = h(b, j) - h(b, i - 1);
+    return (int) (cnt % 2);
+  }
+
+  private long h(int[] b, int v) {
+    int j = b.length - 1;
+    long res = 0;
+    for (int i = 0; i < b.length; i++) {
+      while (j >= 0 && b[i] + b[j] > v) {
+        j--;
+      }
+      if (j <= i) {
+        break;
+      }
+      res += j - i;
+    }
+    return res;
+  }
+}
